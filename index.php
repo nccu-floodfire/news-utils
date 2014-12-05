94a18e2ccae5f76b70f4b1f7fe445ebe2e0fddb0
Youchen Lee <copyleft@utcr.org>
2014-12-05 19:02:29 +0800

    fix a bug of mysql encoding

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ if ($action === null) {
 } else if ($action === 'fetch-count-for-link') {
     // insert fb count to main storagedb
     $Fb = FacebookSocialClient::getInstance($config);
-    $Dbh = new PDO('mysql:host=127.0.0.1;dbname=newsdiff', 'root', '');
+    $Dbh = new PDO('mysql:host=127.0.0.1;dbname=newsdiff', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $Dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $date = $config['date'];
     $ts_start = strtotime($date);
@@ -141,7 +141,7 @@ if ($action === null) {
     exit();
     // TBD
 } else if ($action === 'insert-fb-count-to-serial-db') {
-    $Dbh = new PDO('mysql:host=127.0.0.1;dbname=newsdiff', 'root', '');
+    $Dbh = new PDO('mysql:host=127.0.0.1;dbname=newsdiff', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $Dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $date = $config['date'];
     $ts_start = strtotime($date);
